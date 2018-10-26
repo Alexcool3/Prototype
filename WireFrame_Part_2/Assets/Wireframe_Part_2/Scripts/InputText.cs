a53444ae7a70f55b96fbb48b8d56a7b3004d2a5d
Alexcool3 <Alex33@live.dk>
2018-10-26 09:34:18 +0200

    Script Update
    Search Script
    InputSearch Script

---
+++ WireFrame_Part_2/Assets/Wireframe_Part_2/Scripts/InputText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputText : MonoBehaviour {
+
+    public Text text;
+    public string inputText;
+    public static string[] validSearch = {"1"};
+
+	// Use this for initialization
+	void Start () {
+        text.text = inputText;
+        
+	}
+	
+    void EnterText()
+    {
+
+    }
+	
+}
---
+++ WireFrame_Part_2/Assets/Wireframe_Part_2/Scripts/Search.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Search : MonoBehaviour {
+
+    private bool valid; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void ButtonClicked()
+    {
+
+        if (valid == true)
+        {
+
+        }
+
+    }
+
+}
