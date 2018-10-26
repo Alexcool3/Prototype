31bae0f3aaf5eec4106f1e3df2749c4c5e906a99
Tobias Adamsen <tobiasadamsen@hotmail.com>
2018-10-26 10:28:52 +0200

    Exit button and canvas fixes
    hej

---
+++ WireFrame_Part_2/Assets/Wireframe_Part_2/Scripts/quiitScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quiitScript : MonoBehaviour {
+
+    private void Update()
+    {
+        
+        //QuitGame();
+    }
+    public void ButtonClicked()
+    {
+        
+        QuitGame();
+    }
+    void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+        //Just to make sure its working
+    }
+}
